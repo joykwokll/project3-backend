4242e039298370ed4d3d571bae967c53404e9d2d
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-02 15:46:39 +0800

    update

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -1,4 +1,54 @@
 
+// const express = require('express');
+// const Holiday = require("../models/Holiday")
+// const router = express.Router();
+
+// router.get("/seed", async (req, res) => {
+
+//   const holidays = [
+//     {
+//       name: "New Year's Day",
+//     },
+//     {
+//       name: "Good Friday",
+//     }
+//   ]
+//   await Holiday.deleteMany({});
+//   await Holiday.insertMany(holidays)
+//   res.json(holidays)
+// })
+
+// router.get('/', (req, res) => {
+//   res.send('holidays');
+// })
+
+// // //* Create Route
+// // router.post("/", (req, res) => {
+// //   //* extract the data from req.body
+// //   req.body = { name: "My birthday", likes:888 }
+// //   //* save the data using moongoose into database
+  
+// //   const holiday = Holiday.save(req.body);
+
+// // //   res.send(holiday); 
+// //   res.json(holiday); 
+// // })
+
+// //* Create Route
+// router.post("/", async (req, res) => {
+//     console.log("body", req.body)
+//     try {
+//       const createdHoliday = await Holiday.create(req.body);
+//       res.status(200).send(createdHoliday);
+//     } catch (error) {
+//       res.status(400).json({ error: error.message });
+//     };
+//   });
+  
+//   module.exports = router;
+
+
+
 const express = require('express');
 const Holiday = require("../models/Holiday")
 const router = express.Router();
@@ -16,11 +66,33 @@ router.get("/seed", async (req, res) => {
   await Holiday.deleteMany({});
   await Holiday.insertMany(holidays)
   res.json(holidays)
-//   res.send(holidays)
 })
 
 router.get('/', (req, res) => {
   res.send('holidays');
 })
+
+//* Index Route
+router.get('/', (req, res) => {
+    Holiday.find()
+      .then(holidays => {
+        res.json(holidays)
+      })
+      .catch(err => {
+        res.json(err)
+      })
+  })
+  
+
+//* Create Route
+router.post("/", async (req, res) => {
+  console.log("body", req.body)
+  try {
+    const createdHoliday = await Holiday.create(req.body);
+    res.status(200).send(createdHoliday);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  };
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,49 @@
+// require('dotenv').config()
+// const cors = require('cors')
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const HolidayController = require("./controllers/holidayController")
+
+// const app = express();
+// const PORT = process.env.PORT ?? 2000
+// const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost:27017/holidays"
+
+// // Error / Disconnection
+// mongoose.connection.on("error", (err) =>
+//   console.log(err.message + " is Mongod not running?")
+// );
+// mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+// //...farther down the page
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+// });
+// mongoose.connection.once("open", () => {
+//   console.log("connected to mongoose...");
+// });
+
+// app.use(cors())
+// app.use(express.json());
+// app.use("/api/holidays", HolidayController)
+
+
+// app.get("/", (req, res) => {
+//     res.send('Hi 2');
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// })
+
 require('dotenv').config()
-const cors = require('cors')
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const HolidayController = require("./controllers/holidayController")
 
 const app = express();
-const PORT = process.env.PORT ?? 2000
+const PORT = process.env.PORT ?? 3002
 const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost:27017/holidays"
 
 // Error / Disconnection
@@ -23,7 +61,8 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 app.use("/api/holidays", HolidayController)
 
 
