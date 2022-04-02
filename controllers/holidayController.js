e3c12c2b051e683906e65dc8757c29ee884fb943
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-02 16:54:28 +0800

    TT

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -48,6 +48,62 @@
 //   module.exports = router;
 
 
+// const express = require('express');
+// const Holiday = require("../models/Holiday")
+// const router = express.Router();
+
+// router.get("/seed", async (req, res) => {
+
+//   const holidays = [
+//     {
+//       name: "New Year's Day",
+//     },
+//     {
+//       name: "Good Friday",
+//     }
+//   ]
+//   await Holiday.deleteMany({});
+//   await Holiday.insertMany(holidays)
+//   res.json(holidays)
+// })
+
+// //* Index Route
+// router.get('/', (req, res) => {
+//   Holiday.find()
+//     .then(holidays => {
+//       res.json(holidays)
+//     })
+//     .catch(err => {
+//       res.json(err)
+//     })
+// })
+
+// //* Create Route
+// router.post("/", async (req, res) => {
+//   console.log("body", req.body)
+//   try {
+//     const createdHoliday = await Holiday.create(req.body);
+//     res.status(200).send(createdHoliday);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   };
+// });
+
+// //* Delete Route
+// router.delete("/:id", async (req, res) => {
+//   await Holiday.findByIdAndRemove(req.params.id);
+//   res.json({ message: "Holiday Deleted" });
+// });
+
+// //*Put route
+// router.put("/:id", async (req, res) => {
+//   await Holiday.findByIdAndUpdate(req.params.id, req.body);
+//   res.json({ message: "Holiday Updated" });
+// });
+
+// module.exports = router;
+
+
 
 const express = require('express');
 const Holiday = require("../models/Holiday")
@@ -68,21 +124,16 @@ router.get("/seed", async (req, res) => {
   res.json(holidays)
 })
 
-router.get('/', (req, res) => {
-  res.send('holidays');
-})
-
 //* Index Route
 router.get('/', (req, res) => {
-    Holiday.find()
-      .then(holidays => {
-        res.json(holidays)
-      })
-      .catch(err => {
-        res.json(err)
-      })
-  })
-
+  Holiday.find()
+    .then(holidays => {
+      res.json(holidays)
+    })
+    .catch(err => {
+      res.json(err)
+    })
+})
 
 //* Create Route
 router.post("/", async (req, res) => {
@@ -97,9 +148,14 @@ router.post("/", async (req, res) => {
 
 //* Delete Route
 router.delete("/:id", async (req, res) => {
-    await Holiday.findByIdAndRemove(req.params.id);
-    res.json({ message: "Holiday Deleted" });
-  });
-  
+  await Holiday.findByIdAndDelete(req.params.id);
+  res.json({ message: "Holiday Deleted" });
+});
+
+//*Put route
+router.put("/:id", async (req, res) => {
+  await Holiday.findByIdAndUpdate(req.params.id, req.body);
+  res.json({ message: "Holiday Updated" });
+});
 
 module.exports = router;
